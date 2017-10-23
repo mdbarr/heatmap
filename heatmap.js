f20ac5eb40f560adfc77a632b0c72f80177a6623
mbarr <mbarr@onshape.com>
2017-10-23 14:24:31 -0400

    Use floating point increments for alpha, more efficient color setting.

--- heatmap.js
+++ heatmap.js
@@ -9,11 +9,10 @@ var PNG = require('pngjs').PNG;
 const width = 2560;
 const height = 1440;
 const channels = 4;
-const length = width * height * channels;
+const increment = 0.01;
 
-const increment = 2;
-
-const data = new Uint8Array(length).fill(0);
+const data = new Buffer(width * height * channels).fill(0);
+const alpha = new Float32Array(width * height).fill(0);
 
 const lineReader = readline.createInterface({
   input: fs.createReadStream('rectangles.csv')
@@ -24,29 +23,35 @@ lineReader.on('line', function (line) {
 
   for (let y = coords[1]; y < coords[3]; y++) {
     for (let x = coords[0]; x < coords[2]; x++) {
-      const position = (x * channels) + (y * width * channels);
-
-      // color set to #0073B1
-      // data[position] = 0 is done by the fill
-      data[position + 1] = 115;
-      data[position + 2] = 177;
-
-      const alpha = data[position + 3];
-      data[position + 3] = Math.min(alpha + increment, 255);
+      const alphaPosition = x + (y * width);
+      alpha[alphaPosition] += increment;
     }
   }
 });
 
 lineReader.on('close', function() {
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      const alphaPosition = x + (y * width);
+      if (alpha[alphaPosition]) {
+        const position = (x * channels) + (y * width * channels);
+
+        // #0073B1 - 0, 115, 177
+        data[position] = 0;
+        data[position + 1] = 115;
+        data[position + 2] = 177;
+        data[position + 3] = Math.min(alpha[alphaPosition] * 255, 255);
+      }
+    }
+  }
+
   const png = new PNG();
   png.width = width;
   png.height = height;
-  png.data = new Buffer(data);
-
-  png.pack().pipe(fs.createWriteStream('out.png'));
+  png.data = data;
 
   const options = { colorType: 6 };
 
-  const buffer = PNG.sync.write(png, options);
-  fs.writeFileSync('heatmap.png', buffer);
+  const pngData = PNG.sync.write(png, options);
+  fs.writeFileSync('heatmap.png', pngData);
 });
